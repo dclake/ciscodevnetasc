70ce48d871da6fd83f8c1d557f18849301c50e09
D.C. Lake <dclake@gmail.com>
2021-02-13 07:16:42 -0400

    Added code to list_orgs function

--- merakiorgs.py
+++ merakiorgs.py
@@ -4,19 +4,28 @@ import json
 from pprint import pprint
 
 meraki_api_key = "8f90ecec4fca692f606092279f203c6020ca011c"
-url =  "https://api.meraki.com/api/v0/organizations"
+base_url =  "https://api.meraki.com/api/v0/"
 headers = {
         "X-Cisco-Meraki-API-Key": meraki_api_key,
     }
-#orgs = requests.get(url,headers=headers)
-#print(orgs)
-#print(json.dumps(orgs.json(), indent=4))
+# Creates URI for making API call, 
+# queries API and processes as json
+# Finally prints list of organizations
+def list_orgs():
+	print("\n","="*8, "Organization List", "="*8)
+	path = "organizations"
+	url = base_url+path 
+	#return url 
+	response = requests.request('GET', url=url, headers=headers)
+	json_responce = json.loads(response.text)
+	for item in json_responce:
+		print("ID:", item["id"])
+		print("Name:", item["name"])
+		print()
 
-response = requests.request('GET', url=url, headers=headers)
-json_responce = json.loads(response.text)
-# print(response.text)
-print()
-print("="*5,"Lisiting Organizations","="*5)
-for item in json_responce:
-	print("ID:",item["id"])
-	print("Name:",item["name"])
+		
+		
+
+	
+
+list_orgs()
