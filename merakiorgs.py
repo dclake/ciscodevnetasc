2bcd9d213851c6267146f9cf4274ed2b0553a4b2
D.C. Lake <dclake@gmail.com>
2021-02-13 21:35:02 -0400

    Added listing Networks

--- merakiorgs.py
+++ merakiorgs.py
@@ -11,21 +11,54 @@ headers = {
 # Creates URI for making API call, 
 # queries API and processes as json
 # Finally prints list of organizations
+orgs = []
+org_choice = ""
+org_id =""
 def list_orgs():
 	print("\n","="*8, "Organization List", "="*8)
 	path = "organizations"
 	url = base_url+path 
-	#return url 
 	response = requests.request('GET', url=url, headers=headers)
 	json_responce = json.loads(response.text)
+	n = 0
 	for item in json_responce:
-		print("ID:", item["id"])
-		print("Name:", item["name"])
+		orgs.append(item)
+		print("\n", n, "ID:", item["id"],"Name:", item["name"])
 		print()
+		n +=1
 
+def choose_org():
+	print("Choose an organization: ")
+	org_num = int(input())
+	if org_num <= len(orgs)-1:
+		org_choice = orgs[org_num]
+		org_id = org_choice["id"]
+		print()
+		print("You chose the {name} organization.".format(name=org_choice["name"]))
+		list_networks(org_id)
+	else:
+		print("Invalid entry!!")
+	
 		
-		
+def list_networks(org_choice):
+	print("\n","="*8, "Network List","="*8)
+	path = "organizations/"+org_choice+"/networks"
+	url = base_url+path
+	print(url)
+	response = requests.request('GET', url=url, headers=headers)
+	json_responce = json.loads(response.text)
+	n = 0
+	for item in json_responce:
+		orgs.append(item["id"])
+		print("\n", n, "ID:", item["id"],"Name:", item["name"], "Time Zone:", item["timeZone"])
+		print("Products:",item["productTypes"])
+		print()
+		n +=1
 
 	
 
 list_orgs()
+choose_org()
+print(org_id)
+#print(orgs)
+#print(list_networks(orgs))
