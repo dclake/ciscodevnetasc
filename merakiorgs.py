c95636328c01d7755540f0706af7fa0a1b552f92
D.C. Lake <dclake@gmail.com>
2021-02-15 00:09:46 +0000

    Displays Devices in Network

--- merakiorgs.py
+++ merakiorgs.py
@@ -4,7 +4,8 @@ from pprint import pprint
 
 #meraki_api_key = "8f90ecec4fca692f606092279f203c6020ca011c"
 meraki_api_key = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
-base_url =  "https://api.meraki.com/api/v0/"
+#meraki_api_key = "a556c363ae22547d3e2979f02b22247780020fc1"
+base_url =  "https://api.meraki.com/api/v1/"
 headers = {
         "X-Cisco-Meraki-API-Key": meraki_api_key,
     }
@@ -24,6 +25,7 @@ def list_orgs():
 	url = base_url+path 
 	response = requests.request('GET', url=url, headers=headers)
 	json_responce = json.loads(response.text)
+	print(json_responce)
 	n = 0
 	for item in json_responce:
 		orgs.append(item)
@@ -41,7 +43,6 @@ def choose_org():
 		list_networks(org_id)
 	else:
 		print("Invalid entry!!")
-	
 		
 def list_networks(org_choice):
 	print("\n","="*8, "Network List","="*8)
@@ -52,27 +53,44 @@ def list_networks(org_choice):
 	json_responce = json.loads(response.text)
 	n = 0
 	for item in json_responce:
-		nets.append(item["id"])
+		#nets.append(item["id"])
+		nets.append(item)
 		print(str(n)+". ", "ID:", item["id"],"Name:", item["name"], "Time Zone:", item["timeZone"])
 		print("    Products:",item["productTypes"])
 		n +=1
 
 def choose_net():
-	print("hello")
+	#print(nets)
 	print("Which network would you like to access?\n")
 	net_num = int(input())
 	if net_num <= len(nets)-1:
-		net_choice = nets[org_num]
+		net_choice = nets[net_num]
 		net_id = net_choice["id"]
-		print()
-		print("Accessing the {name} organization.".format(name=org_choice["name"]))
-		list_networks(org_id)
+		print(net_id)
+		print("Accessing the {name} network.".format(name=net_choice["name"]))
+		list_devices(net_id)
 	else:
 		print("Invalid entry!!")	
 
+def list_devices(net_choice):
+	print("\n","="*8, "Device List","="*8)
+	path = "networks/"+net_choice+"/devices"
+	url = base_url+path
+	print(url)
+	response = requests.request('GET', url=url, headers=headers)
+	json_responce = json.loads(response.text)
+	#print(json_responce)
+	n = 0
+	for item in json_responce:
+	#	nets.append(item["id"])
+		print(str(n)+". ", "Serial:", item["serial"],"MAC:", item["mac"], "IP:", item["lanIp"], "Model:", item["model"])
+	#	print("    Products:",item["productTypes"])
+		n +=1
+
 list_orgs()
 choose_org()
-print(org_id)
+choose_net()
+#print(org_id)
 #print(orgs)
 #print(list_networks(orgs))
 
